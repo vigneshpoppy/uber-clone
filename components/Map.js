593f62f153022d1afac205d06130dc25805769bf
Vignesh C <poppyseed6464@outlook.com>
2022-05-03 14:47:10 +0530

    Added Map

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect, useContext } from 'react'
+import mapboxgl from 'mapbox-gl'
 
+const style = {
+    wrapper: `flex-1 h-full w-full`,
+  }
+  
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
-
-export default Map
+   
+  
+    useEffect(() => {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+        center: [-99.29011, 39.39172],
+        zoom: 3,
+      })
+  
+     
+      
+    }, [])
+  
+    const addToMap = (map, coordinates) => {
+      const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+    }
+  
+    return <div className={style.wrapper} id='map' />
+  }
+  
+  export default Map
+  
